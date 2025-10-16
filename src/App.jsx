55b8bcd1f7d7b3a7c2d84897ee73db36e9765bd9
regret546 <iregret546@gmail.com>
2025-10-16 22:52:26 +0800

    Add contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Certificates from "./sections/Certificates";
 import About from "./sections/About";
 import Project from "./sections/Project";
 import Experience from "./sections/Experience";
+import Contact from "./sections/Contact";
 
 import "./style.css";
 
@@ -19,6 +20,7 @@ function App() {
           <Project />
           <Certificates />
           <Experience />
+          <Contact />
         </div>
       </div>
     </>
---
+++ src/sections/Contact.jsx
@@ -0,0 +1,22 @@
+const Contact = () => {
+  return (
+    <div className=" w-[60%]  mb-5 grid gap-4">
+      <h2 className="font-extrabold text-[3.5rem] text-center w-full text-accent-blue">
+        Contact.
+      </h2>
+      <p className="text-center">
+        Shoot me an email if you want to connect! You can also find me on{" "}
+        <span className="text-accent-blue font-medium cursor-pointer">
+          Linkedin
+        </span>{" "}
+        or{" "}
+        <span className="text-accent-blue font-medium cursor-pointer">
+          Facebook
+        </span>{" "}
+        if that's more your speed.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
